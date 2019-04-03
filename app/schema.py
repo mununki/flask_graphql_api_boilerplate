e8864bd8f9332dcfb835c417172d7713ac6b982e
mattdamon108 <woonki.moon@gmail.com>
2019-04-03 16:39:56 +0900

    change profile

--- app/schema.py
+++ app/schema.py
@@ -1,11 +1,26 @@
 import graphene
 from graphene_sqlalchemy import SQLAlchemyObjectType
 from flask_jwt_extended import (JWTManager, jwt_required, create_access_token, decode_token)
-from flask import request
+from functools import wraps
 
 from app import db
 from app import jwt
 from app.models import MyUser
+
+
+def signin_required(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        try:
+            user_id = decode_token(args[1].context.headers.get('authorization'))['identity']
+        except Exception:
+            user_id = None
+        if user_id:
+            args = list(args)
+            args.append(user_id)
+            args = tuple(args)
+        return fn(*args, **kwargs)
+    return wrapper
 
 
 class MyUserType(SQLAlchemyObjectType):
@@ -22,8 +37,8 @@ class GetMyProfile(graphene.ObjectType):
 class Query(graphene.ObjectType):
     me = graphene.Field(GetMyProfile)
 
-    def resolve_me(self, info):
-        user_id = decode_token(info.context.headers.get('authorization'))['identity']
+    @signin_required
+    def resolve_me(self, info, user_id=None):
         if user_id:
             user = MyUser.query.filter_by(id=user_id).first()
             if user:
@@ -78,9 +93,34 @@ class SignInResponse(graphene.Mutation):
             return SignInResponse(ok=False, error="Not existing user", token=None)
 
 
+class ChangeProfileResponse(graphene.Mutation):
+    ok = graphene.Boolean()
+    error = graphene.String()
+    user = graphene.Field(MyUserType)
+
+    class Arguments:
+        bio = graphene.String()
+
+    @signin_required
+    def mutate(self, info, user_id=None, bio=None, **kwargs):
+        if user_id:
+            user = MyUser.query.filter_by(id=user_id).first()
+            if user:
+                user.bio = bio
+                db.session.commit()
+                return ChangeProfileResponse(ok=True, error=None, user=user)
+            else:
+                return ChangeProfileResponse(ok=False, error="Not existing user", user=None)
+        else:
+            return ChangeProfileResponse(ok=False, error="Login required", user=None)
+            
+
+
+
 class Mutation(graphene.ObjectType):
     sign_up = SignUpResponse.Field()
     sign_in = SignInResponse.Field()
+    change_profile = ChangeProfileResponse.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
